f0ac8e2106f73fa143d6bc3ccc19ad9b2636d1b2
Gabriel <Gabriel8819@hotmail.com>
2020-08-10 19:48:12 -0400

    Add Datasource - resources.xml, web.xml, code in servlet

--- tomeeJpa/src/main/java/com/devpixelart/controllers/HomeControllers.java
+++ tomeeJpa/src/main/java/com/devpixelart/controllers/HomeControllers.java
@@ -2,11 +2,21 @@ package com.devpixelart.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
 
 @WebServlet("/home")
 public class HomeControllers extends HttpServlet{
@@ -17,6 +27,28 @@ public class HomeControllers extends HttpServlet{
 		PrintWriter out = res.getWriter();
 		
 		out.println("Tomcat ee servlet home");
+		
+		
+		Context ctx;
+		try {
+			ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/myDs");
+			
+			Connection conn = ds.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet set = stmt.executeQuery("select * from users;");
+			
+			while(set.next()) {
+				out.println(set.getString(2));
+			}
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		
 		
